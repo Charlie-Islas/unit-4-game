61f38574afa5ed4902287d4f138be3449b54942b
Charlie <charlie.islas@gmail.com>
2018-09-11 18:47:36 -0500

    uploading with all validations and bootstrap alerts working properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,10 +10,19 @@ window.onload=function(){
     var diamondVal=0;
     var randomNum=0;
     var userScore=0;
-    var proceed=false;
+    var roundWin=false;
+    $("#loseAlert").hide();
+    $("#winAlert").hide();
+   
 
+    //function to generate random numbers for all the crystals and the game's goal
     function genValues(){
 
+        roundWin=false;
+        userScore=0;   
+     
+         //aqui me quede, intentando resetear la variable booleana para que me deje volver a jugar
+        $("#currentScore").html(userScore);
         randomNum=Math.floor(Math.random() * 101) + 19;
         $("#randomNum").html(randomNum);
 
@@ -21,6 +30,11 @@ window.onload=function(){
         saphireVal=Math.floor(Math.random() * 11) + 1;
         yellowDVal=Math.floor(Math.random() * 11) + 1;
         diamondVal=Math.floor(Math.random() * 11) + 1;
+
+        console.log(rubyVal);
+        console.log(saphireVal);
+        console.log(yellowDVal);
+        console.log(diamondVal);
         checkValues();
 
     }
@@ -30,8 +44,9 @@ window.onload=function(){
 
         if(rubyVal!==saphireVal&&rubyVal!==yellowDVal&&rubyVal!==diamondVal&&saphireVal!==yellowDVal&&saphireVal!==diamondVal&&yellowDVal!==diamondVal){
             clickCrystals();
+            hideAlert=true;            
         }
-        else{genValues();}
+        else{ genValues(); }
 
     }
 
@@ -40,26 +55,89 @@ window.onload=function(){
         if(!gameOver){
 
             $("#ruby").on("click", function(){
+                if(!roundWin){
                 userScore+=rubyVal;
                 $("#currentScore").html(userScore);
+                checkWin();}
             });
 
             $("#saphire").on("click", function(){
+                if(!roundWin){
                 userScore+=saphireVal;
                 $("#currentScore").html(userScore);
+                checkWin();}
             });
 
             $("#yellowDiamond").on("click", function(){
+                if(!roundWin){
                 userScore+=yellowDVal;
                 $("#currentScore").html(userScore);
+                checkWin();}
             });
 
             $("#diamond").on("click", function(){
+                if(!roundWin){
                 userScore+=diamondVal;
                 $("#currentScore").html(userScore);
+                checkWin();}
             });
 
         }
+        
+        
+
+
+    }
+
+    function checkWin(){
+
+        $("#currentScore").html(userScore);
+
+        if(userScore===randomNum){
+            $("#currentScore").html(userScore);
+            wins++;
+            $("#wins").html(wins);
+            gameOver=true;
+            roundWin=true;
+            $("#winAlert").show();
+
+            document.onkeyup = function (event) {
+
+                $("#winAlert").hide();
+                if(userScore!=0){
+                genValues();}
+        
+                
+
+            } 
+        }
+
+        else if(userScore>randomNum){
+
+
+            $("#currentScore").html(userScore);
+            defeats++;
+            $("#defeats").html(defeats);
+            gameOver=true;
+            roundWin=true;
+            $("#loseAlert").show();
+
+            
+           
+                document.onkeyup = function (event) {
+
+            
+                    $("#loseAlert").hide();
+                    if(userScore!=0){
+                    genValues();}
+                    
+                    
+
+                }
+            
+        }
+    
+
     }
 
     genValues();
